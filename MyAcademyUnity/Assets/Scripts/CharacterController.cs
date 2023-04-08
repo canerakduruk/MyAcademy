90ac39924af40357f744b4becb9df0403e51f22c
Caner Akduruk <canakduruk@gmail.com>
2023-04-09 00:50:44 +0300

    Character Update3

--- MyAcademyUnity/Assets/Scripts/CharacterController.cs
+++ MyAcademyUnity/Assets/Scripts/CharacterController.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody charRb;
+    Animator charAnim;
+    [SerializeField]
+    GameObject hedefBakis;
+
+    float hiz = 5;
+    float ziplamaHizi = 200;
+    bool zipla,ziplandiMi;
+    float hizHorizontal,hizVertical,aci;
     void Start()
     {
+        charRb = GetComponent<Rigidbody>();
+        charAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        hizHorizontal = Input.GetAxis("Horizontal");
+        hizVertical = Input.GetAxis("Vertical");
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && ziplandiMi == false)
+        {
+            zipla = true;
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
+        Kosmak(hizHorizontal,hizVertical);
+        Ziplamak(zipla);
+        Donmek(hizHorizontal,hizVertical);
+        
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        
+        if (other.transform.CompareTag("Zemin"))
+        {
+            ziplandiMi = false;
+            charAnim.SetBool("Zipliyor",false);
+        }
+        
+    }
+
+    void Kosmak(float hizHorizontalKos,float hizVerticalKos)
+    {
+        if(hizHorizontalKos !=0 || hizVerticalKos !=0)
+        {
+            charRb.velocity = new Vector3(hizHorizontalKos*hiz,charRb.velocity.y,hizVerticalKos*hiz);
+            charRb.transform.LookAt(hedefBakis.transform);
+            charAnim.SetBool("Kosuyor",true);
+        }
+        else
+        {
+            charAnim.SetBool("Kosuyor",false);
+        }
+    }
+
+    void Ziplamak(bool ziplaPar)
+    {
+        if (ziplaPar)
+        {
+            ziplandiMi = true;
+            zipla = false;
+            charRb.AddForce(Vector3.up*ziplamaHizi);
+            charAnim.SetBool("Zipliyor",true);
+        }
+    }
+    void Donmek(float hizHorizontalPar, float hizVerticalPar)
+    {
+        if (hizHorizontalPar > 0.1 || hizHorizontalPar < -0.1 || hizVerticalPar > 0.1 || hizVerticalPar < -0.01)
+        {
+            hedefBakis.transform.position = new Vector3(gameObject.transform.position.x+10*hizHorizontalPar,gameObject.transform.position.y,gameObject.transform.position.z+10*hizVerticalPar);
+
+        }
+        
         
     }
 }
